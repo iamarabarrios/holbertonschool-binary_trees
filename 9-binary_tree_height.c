d55d3c2adca1f45c29780f77b53a0472bcadae69
Kiwizacxs <brian.deleon2016@outlook.com>
2023-09-03 15:06:39 +0000

    tree_height

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+    size_t height = 0;
+
+    if (tree == NULL)
+    {
+        return (0);
+    }
+    while (tree->right != NULL)
+    {
+        tree = tree->right;
+        height++;
+    }
+    return (height);
+}
