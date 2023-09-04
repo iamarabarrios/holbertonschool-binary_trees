6a211d1769fa8a2cc34cdd4ead436f07dd8cb524
iamara <iamarabarrios457@gmail.com>
2023-09-04 05:31:47 -0700

    exercise 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -16,25 +16,16 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	return (0);
 	}
 
-	while (tree->right != NULL)
+	if (tree->right)
 	{
-		right_height++;
-		tree = tree->right;
+		right_height += 1 + binary_tree_height(tree->right);
 	}
 
-	while (tree->left != NULL)
+	if (tree->left)
 	{
-		left_height++;
-		tree = tree->left;
+		left_height += 1 + binary_tree_height(tree->left);
 	}
-	if (left_height > right_height)
-	{
-		return (left_height);
-	}
-	else
-	{
-	return (right_height);
-	}
-/**return (left_height > right_height ? left_height : right_height);*/
+
+	return (left_height > right_height ? left_height : right_height);
 }
 
