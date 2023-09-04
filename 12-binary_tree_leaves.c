5f8600a90261719d4aa3856fc36922c68a89090b
Kiwizacxs <brian.deleon2016@outlook.com>
2023-09-04 16:24:10 +0000

    num of leaves

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,31 +1,35 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_leaves - Function that counts the leaves
+ * in a binary tree.
+ * @tree: pointer to the root node of the tree to count
+ * the number of leaves.
+ * Return: num of the leaves.
+ */
+
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-    size_t leaves = 0;
+	size_t leaves = 0;
 
-    if (tree == NULL)
-    {
-        return (0);
-    }
-    if (tree->left == NULL)
-    {
-        return (1);
-    }
-    if (tree->right == NULL)
-    {
-        return (1);
-    }
-    if (tree->right)
+	if (tree == NULL)
 	{
-        binary_tree_leaves(tree->right);
-        leaves++;
+		return (0);
+	}
+	if (tree->left == NULL || tree->right == NULL)
+	{
+		return (1);
+	}
+	if (tree->right)
+	{
+		binary_tree_leaves(tree->right);
+		leaves++;
 	}
 
 	if (tree->left)
 	{
-        binary_tree_leaves(tree->left);
-        leaves++;
+		binary_tree_leaves(tree->left);
+		leaves++;
 	}
-    return (leaves);
+	return (leaves);
 }
---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_nodes - Function that counts the
+ * nodes with at least 1 child in a binary tree.
+ * @tree: pointer to the root node of the tree to
+ * count the number of nodes.
+ * Return: num of nodes with at least 1 child in a binary tree.
+ */
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	if ()
+}
