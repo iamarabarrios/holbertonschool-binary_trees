56f3f2f8fe0327d52cd1f2dbb00516176c094a73
Kiwizacxs <brian.deleon2016@outlook.com>
2023-09-05 00:01:41 +0000

    balance factor

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,32 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_balance - Function that measures the
+ * balance factor of a binary tree.
+ * @tree: pointer to the root node of the tree to
+ * measure the balance factor.
+ * Return: diference of height of the sub trees.
+ */
+
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	int l_height = 0;
+	int r_height = 0;
+
+	if (tree == NULL)
+	{
+		return (0);
+	}
+
+	if (tree->right)
+	{
+		r_height += 1 + binary_tree_balance(tree->right);
+	}
+
+	if (tree->left)
+	{
+		l_height += 1 + binary_tree_balance(tree->left);
+	}
+
+	return (l_height - r_height);
+}
